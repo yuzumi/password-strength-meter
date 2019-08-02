5d902bbd7badd477e64f61a11f6020edcd21762b
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-02 11:45:36 +0300

    Add PasswordStrengthMeter component

---
+++ src/components/PasswordStrengthMeter/index.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from 'react';
+import zxcvbn, { ZXCVBNResult, ZXCVBNScore } from 'zxcvbn';
+import './index.css';
+
+export type PasswordStrengthLevels = 'Very weak' | 'Weak' | 'Fair' | 'Good' | 'Strong';
+
+export const getPasswordStrengthLevel = (score: ZXCVBNScore): PasswordStrengthLevels => {
+  switch (score) {
+    case 0: return 'Very weak';
+    case 1: return 'Weak';
+    case 2: return 'Fair';
+    case 3: return 'Good';
+    case 4: return 'Strong';
+    default: return 'Very weak';
+  }
+};
+
+export interface PasswordStrengthMeterProps {
+  password: string;
+};
+
+const PasswordStrengthMeter: FunctionComponent<PasswordStrengthMeterProps> = ({ password }) => {
+  const testedResult: ZXCVBNResult = zxcvbn(password);
+  const strengthLevel: PasswordStrengthLevels = getPasswordStrengthLevel(testedResult.score);
+
+  return (
+    <div className="password-strength-meter">
+      <progress
+        className="password-strength-meter__strength"
+        value={testedResult.score}
+        max={4}
+      />
+      {password && (
+        <p className="password-strength-meter__label">
+          Password strength: {strengthLevel}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default PasswordStrengthMeter;
