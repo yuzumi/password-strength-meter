99c5f0a01caba2bc95336e07f1aa5d11b0ed8c4c
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-04 15:12:55 +0300

    Prettify code

--- src/components/PasswordStrengthMeter/index.tsx
+++ src/components/PasswordStrengthMeter/index.tsx
@@ -2,26 +2,43 @@ import React, { FunctionComponent } from 'react';
 import zxcvbn, { ZXCVBNResult, ZXCVBNScore } from 'zxcvbn';
 import './index.css';
 
-export type PasswordStrengthLevels = 'Very weak' | 'Weak' | 'Fair' | 'Good' | 'Strong';
+export type PasswordStrengthLevels =
+  | 'Very weak'
+  | 'Weak'
+  | 'Fair'
+  | 'Good'
+  | 'Strong';
 
-export const getPasswordStrengthLevel = (score: ZXCVBNScore): PasswordStrengthLevels => {
+export const getPasswordStrengthLevel = (
+  score: ZXCVBNScore
+): PasswordStrengthLevels => {
   switch (score) {
-    case 0: return 'Very weak';
-    case 1: return 'Weak';
-    case 2: return 'Fair';
-    case 3: return 'Good';
-    case 4: return 'Strong';
-    default: return 'Very weak';
+    case 0:
+      return 'Very weak';
+    case 1:
+      return 'Weak';
+    case 2:
+      return 'Fair';
+    case 3:
+      return 'Good';
+    case 4:
+      return 'Strong';
+    default:
+      return 'Very weak';
   }
 };
 
 export interface PasswordStrengthMeterProps {
   password: string;
-};
+}
 
-const PasswordStrengthMeter: FunctionComponent<PasswordStrengthMeterProps> = ({ password }) => {
+const PasswordStrengthMeter: FunctionComponent<PasswordStrengthMeterProps> = ({
+  password
+}) => {
   const testedResult: ZXCVBNResult = zxcvbn(password);
-  const strengthLevel: PasswordStrengthLevels = getPasswordStrengthLevel(testedResult.score);
+  const strengthLevel: PasswordStrengthLevels = getPasswordStrengthLevel(
+    testedResult.score
+  );
 
   return (
     <div className="password-strength-meter">
